66347773056d0f7d23a69d86d9de46833c75aa8f
ANOSH <calebe11anos@gmail.com>
2026-08-04 10:34:33 -0300

    add parse nginx log

--- src/sentinel/parsers/nginx_parser.py
+++ src/sentinel/parsers/nginx_parser.py
@@ -3,24 +3,14 @@ import yaml
 
 from base_parser import BaseParser
 
-def load_patterns(path, category):
-    with open(path) as f:
-        raw = yaml.safe_load(f)
-
-    compiled = {}
-
-    for event_type, data in raw[category].items():
-        compiled[data["keyword"]] = (event_type, re.compile(data["regex"]))
-        return compiled
-
 class NginxParseLog(BaseParser):
-    HEADER = re.compile(r"(?P<ip>\d+).+\[(?P<day>\d{2})/(?P<month>\w{3})/(?P<year>\d{4}):(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})\s+(?P<tz>[+-]\d{4})\]\s+\"(?P<method>\w+)\s+/(?P<URL>.+)/(?P<version>\d\.\d)\"\s+(?P<stcd>)\s+(?P<bytes>\d+)\"(?P<subd>.+)\"")
+    HEADER = re.compile(r"(?P<ip>[\d.]+).+\[(?P<day>\d{2})/(?P<month>\w{3})/(?P<year>\d{4}):(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})\s+(?P<tz>[+-]\d{4})\]\s+\"(?P<method>\w+)\s+(?P<url>\S+)\s+HTTP/(?P<version>\d\.\d)\"\s+(?P<stcd>\d{3})\s+(?P<bytes>\d+)\s+\"(?P<referrer>[^\"]*)\"\s+\"(?P<user_agent>[^\"]*)")
 
     #def __init__(self, patterns_path="config/log_patterns.yaml"):
 
     def parse(self, filepath):
         
-        events = {}
+        events = []
         
         with open(filepath) as f:
             for line in f:
@@ -29,4 +19,25 @@ class NginxParseLog(BaseParser):
 
                 if not match:
                     continue
+
+                dados = match.groupdict()
+                dados["status_code"] = dados.pop("stcd")
+                dados["timezone"] = dados.pop("tz")
+                
+                events.append(dados)
+
+        return events
+
+if __name__ == "__main__":
+    parser = NginxParseLog()
+    events = parser.parse("datasets/synthetic/nginx_test.log")
+
+    for evento in events:
+        print(evento)
+
+    print(f"\nTotal de eventos: {len(events)}")
+
+
+
+
                 
